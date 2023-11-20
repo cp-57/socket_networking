ad8b651873640b682e0b51f7ce2fa529ced766ca
cp-57 <109479938+cp-57@users.noreply.github.com>
2023-11-19 21:51:14 -0500

    Create tcp_file_transfer_client.py

---
+++ tcp_file_transfer_client.py
@@ -0,0 +1,32 @@
+import socket
+
+def send_file_to_server(host, port, filename):
+    """ Sends a file to the server """
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        try:
+            # Connect to the server
+            client_socket.connect((host, port))
+
+            # Send the filename
+            client_socket.sendall(filename.encode())
+
+            # Open the file and send its contents
+            with open(filename, 'rb') as file:
+                while True:
+                    bytes_read = file.read(4096)
+                    if not bytes_read:
+                        # File transmission is done
+                        break
+                    client_socket.sendall(bytes_read)
+            
+            print(f"File {filename} has been sent.")
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    HOST = '10.10.10.2'  
+    PORT = 5000         
+    FILENAME = 'example.txt'
+
+    send_file_to_server(HOST, PORT, FILENAME)
